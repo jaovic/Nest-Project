6b75e363697cd3923e2b556a224a2d9a15bf4c2a
jaovic <j_victorfernandes@hotmail.com>
2022-04-13 11:22:26 -0300

    ✨

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,11 +1,20 @@
-import { Controller, Delete, Get, Post, Put, Body } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Put,
+  Body,
+  Param,
+  Query,
+} from '@nestjs/common';
 import { createTaskDto } from './dto/create.task.dto';
 
 @Controller('tasks')
 export class TasksController {
   @Get('/get')
-  getTasks(): string {
-    return JSON.stringify({ msg: 'Return Tasks' });
+  getTasks(): { msg: string } {
+    return { msg: 'Return Tasks' };
   }
   @Post('/post')
   createTask(@Body() task: createTaskDto) {
@@ -15,8 +24,9 @@ export class TasksController {
   updateTask(): string {
     return JSON.stringify({ Post: 'Update Task' });
   }
-  @Delete('/delete')
-  deleteTaks(): string {
-    return JSON.stringify({ Post: 'Delete Task' });
+  @Delete('/delete?')
+  deleteTaks(@Query('id') id): string {
+    console.log(id);
+    return JSON.stringify({ Post: `${id}` });
   }
 }
